2dd1503a97c6f2cc99047d6605a7e0962bef060d
linda <frulinsaws@gmail.com>
2025-11-07 10:53:10 +0100

    da

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "linsbucket-221025"
-    key    = "raphael"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "linsbucket-221025"
+#     key    = "raphael"
+#     region = "us-east-1"
+#   }
+# }
